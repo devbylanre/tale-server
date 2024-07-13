47a9e130f52ac5e04691560e0d8592438128292b
codewithaudrey <fantasticaudrey001@gmail.com>
2024-07-13 09:24:02 +0100

    new routes, applo server

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,11 @@ import authTypeDefs from './schemas/authSchema';
 import categoryTypeDefs from './schemas/categorySchema';
 import postTypeDefs from './schemas/postSchema';
 import postResolver from './resolvers/postResolver';
+import commentTypeDefs from './schemas/commentSchema';
+import commentResolver from './resolvers/commentResolver';
+import tokenTypeDefs from './schemas/tokenSchema';
+import tokenResolver from './resolvers/tokenResolver';
+import uploadRoutes from './routes/uploadRoutes';
 
 const startServer = async () => {
   await mongooseConfig();
@@ -27,6 +32,11 @@ const startServer = async () => {
   const userServer = new ApolloServer({
     typeDefs: userTypeDefs,
     resolvers: userResolver,
+  });
+
+  const tokenServer = new ApolloServer({
+    typeDefs: tokenTypeDefs,
+    resolvers: tokenResolver,
   });
 
   const authServer = new ApolloServer({
@@ -44,15 +54,26 @@ const startServer = async () => {
     resolvers: categoryResolver,
   });
 
+  const commentServer = new ApolloServer({
+    typeDefs: commentTypeDefs,
+    resolvers: commentResolver,
+  });
+
   await userServer.start();
   await authServer.start();
   await postServer.start();
   await categoryServer.start();
+  await commentServer.start();
+  await tokenServer.start();
 
   app.use('/users/', expressMiddleware(userServer));
+  app.use('/tokens/', expressMiddleware(tokenServer));
   app.use('/auth/', expressMiddleware(authServer));
   app.use('/posts/', expressMiddleware(postServer));
   app.use('/categories/', expressMiddleware(categoryServer));
+  app.use('/comments/', expressMiddleware(commentServer));
+
+  app.use('/uploads', uploadRoutes);
 
   const PORT = process.env.PORT || 4000;
 
