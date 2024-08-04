27cb59630ee8dcb5342e21b29ed56ee5700daebe
codewithaudrey <fantasticaudrey001@gmail.com>
2024-08-05 00:59:20 +0100

    schema(media): setup

---
+++ src/schemas/mediaSchema.ts
@@ -0,0 +1,29 @@
+const mediaTypeDefs = `#graphql 
+    type Media {
+        _id: ID!
+        hash: String!
+        name: String!
+        path: String!
+        size: Int!
+        uri: String!
+        user: User
+        alt: String
+        createdAt: Float!
+    }
+
+    type Query {
+        media(id: ID!): Media
+        medias: [Media!]
+    }
+
+    input UpdateMediaPayload {
+        name: String
+    }
+
+    type Mutation {
+        deleteMedia(id: ID!): Media
+        updateMedia(id: ID!, payload: UpdateMediaPayload!): Media
+    } 
+`;
+
+export default mediaTypeDefs;
