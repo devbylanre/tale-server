ea681fd6bf7605f48d70e260ecf3e763d9c4e41c
codewithaudrey <fantasticaudrey001@gmail.com>
2024-08-05 01:04:15 +0100

    bug fixes

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import { ApolloServer } from '@apollo/server';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import { expressMiddleware } from '@apollo/server/express4';
-import { GraphQLError } from 'graphql';
+import cookie from 'cookie-parser';
 
 dotenv.config();
 
@@ -21,12 +20,9 @@ import commentTypeDefs from './schemas/commentSchema';
 import commentResolver from './resolvers/commentResolver';
 import tokenTypeDefs from './schemas/tokenSchema';
 import tokenResolver from './resolvers/tokenResolver';
-import uploadRoutes from './routes/uploadRoutes';
-import uploadTypeDefs from './schemas/uploadSchema';
-import uploadResolver from './resolvers/uploadResolver';
-import { upload } from './middleware/multerMiddleware';
-import tokenize from './utils/token';
-import cookie from 'cookie-parser';
+import mediaRoute from './routes/mediaRoute';
+import mediaTypeDefs from './schemas/mediaSchema';
+import mediaResolver from './resolvers/mediaResolver';
 
 const startServer = async () => {
   const typeDefs = [
@@ -36,7 +32,7 @@ const startServer = async () => {
     categoryTypeDefs,
     commentTypeDefs,
     postTypeDefs,
-    uploadTypeDefs,
+    mediaTypeDefs,
   ];
 
   const resolvers = [
@@ -46,14 +42,14 @@ const startServer = async () => {
     postResolver,
     categoryResolver,
     commentResolver,
-    uploadResolver,
+    mediaResolver,
   ];
 
   const app = express();
 
   app.use(express.json());
   app.use(cookie());
-  app.use(cors({ origin: 'http://localhost:3000' }));
+  app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
   await mongooseConfig();
 
@@ -68,16 +64,12 @@ const startServer = async () => {
     '/graphql/',
     expressMiddleware(server, {
       context: async ({ req, res }) => {
-        console.log({ cookies: req.cookies });
-        const token = req.headers.authorization?.split(' ')[1];
-        const user = tokenize.verify(token, process.env.ACCESS_TOKEN as string);
-
-        return { user, req, res };
+        return { req, res };
       },
     })
   );
 
-  app.use('/rest/uploads', uploadRoutes);
+  app.use('/rest/medias', mediaRoute);
 
   const PORT = process.env.PORT || 4000;
 
