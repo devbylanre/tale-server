dd6225b16d5589a3b2a2b1a8b5854a2ce7a29251
devbylanre <devbylanre@gmail.com>
2024-08-23 21:28:18 +0100

    added role model

---
+++ src/models/role.ts
@@ -0,0 +1,15 @@
+import { model, Schema, Types } from 'mongoose';
+
+export type Role = {
+  name: 'admin' | 'author' | 'reader' | 'owner' | 'developer';
+  capabilities: Types.ObjectId[];
+};
+
+const schema = new Schema<Role>({
+  name: { type: String, required: true, unique: true },
+  capabilities: [{ type: String, ref: 'Capabilities' }],
+});
+
+const Roles = model<Role>('Roles', schema);
+
+export default Roles;
