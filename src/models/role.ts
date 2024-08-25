4f0e4cf849bbe04d7e626e3822bb22dcee92c1ad
devbylanre <devbylanre@gmail.com>
2024-08-25 08:21:35 +0100

    added role description

--- src/models/role.ts
+++ src/models/role.ts
@@ -3,11 +3,13 @@ import { model, Schema, Types } from 'mongoose';
 export type Role = {
   _id: Types.ObjectId;
   name: 'admin' | 'author' | 'reader' | 'owner' | 'developer';
+  description: string;
   capabilities: Types.ObjectId[];
 };
 
 const schema = new Schema<Role>({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true, trim: true },
+  description: { type: String, required: true },
   capabilities: [{ type: String, ref: 'Capabilities' }],
 });
 
