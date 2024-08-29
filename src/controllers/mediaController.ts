53822d6943f84d671269bdc4967ffaf2d7c329bc
devbylanre <devbylanre@gmail.com>
2024-08-29 02:31:33 +0100

    refactor: - added new util - records now variable

--- src/controllers/mediaController.ts
+++ src/controllers/mediaController.ts
@@ -1,15 +1,14 @@
 import { Request, Response } from 'express';
-import path from 'path';
-import crypto from 'crypto';
 import { ref, getStorage, uploadBytes, getDownloadURL } from 'firebase/storage';
 import firebaseApp from '../config/firebase';
-import Medias from '../models/media';
-import { Ext, isSupported } from '../utils/file';
+import Medias, { Media } from '../models/media';
+import fileUtil from '../utils/file';
+import random from '../utils/random';
 
 const storage = getStorage(firebaseApp, process.env.FIREBASE_STORAGE_BUCKET);
 
 const multiple = async ({ files, user, query }: Request, res: Response) => {
-  const exts = query.exts as Ext[];
+  const exts = query.exts as string[] | undefined;
 
   try {
     if (!files || !Array.isArray(files)) {
@@ -18,39 +17,37 @@ const multiple = async ({ files, user, query }: Request, res: Response) => {
 
     const medias = await Promise.all(
       files.map(async (file) => {
-        const fileId = crypto.randomBytes(16).toString('hex');
-        const filePath = path.extname(file.originalname);
-        const fileName = path.basename(file.originalname).split('.')[0];
-        const fileRef = ref(storage, `uploads/${fileId}${filePath}`);
+        const id = random.hex(16);
+        const ext = fileUtil.ext(file.originalname);
+        const name = fileUtil.name(file.originalname);
+        const type = fileUtil.type(file.mimetype);
+
+        const fileRef = ref(storage, `uploads/${id}${ext}`);
 
         if (exts && exts.length > 0) {
-          const isValid = isSupported(file.originalname, exts);
-
-          if (!isValid) {
-            throw new Error(
-              `Unsupported file format ${filePath}. Files allowed are: ${exts}`
-            );
-          }
+          fileUtil.isSupported(file.originalname, exts);
         }
 
         const { metadata } = await uploadBytes(fileRef, file.buffer);
-        const URI = await getDownloadURL(fileRef);
+        const uri = await getDownloadURL(fileRef);
 
-        return {
-          hash: fileId,
-          name: fileName,
-          path: metadata.fullPath,
+        const media: Omit<Media, '_id' | 'createdAt' | 'alt'> = {
+          uri: uri,
+          name: name,
+          type: type,
+          uploadedBy: user.id,
           size: metadata.size,
-          user: user.id,
-          uri: URI,
+          path: metadata.fullPath,
         };
+
+        return media;
       })
     );
 
-    const records = medias.map((media) => new Medias(media));
-    await Promise.all(records.map((record) => record.save()));
+    const documents = medias.map((media) => new Medias(media));
+    await Promise.all(documents.map((document) => document.save()));
 
-    return res.status(200).json({ medias: records });
+    return res.status(200).json({ medias: documents });
   } catch (error) {
     res.status(500).json({ message: (error as Error).message });
   }
