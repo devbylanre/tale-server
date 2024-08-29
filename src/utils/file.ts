78e21ef92f673753c217e0fb776ca4a63bb46754
devbylanre <devbylanre@gmail.com>
2024-08-29 02:24:48 +0100

    added new functions for deriving file extension, type, and name

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,23 +1,39 @@
 import path from 'path';
 
-export type Ext =
-  | 'jpg'
-  | 'jpeg'
-  | 'png'
-  | 'svg'
-  | 'gif'
-  | 'mp4'
-  | 'avi'
-  | 'mov'
-  | 'mp3';
+const file = {
+  ext: (originalname: Express.Multer.File['originalname']) => {
+    const ext = path.extname(originalname);
+    return ext;
+  },
 
-const isSupported = (
-  originalname: Express.Multer.File['originalname'],
-  exts: Ext[]
-) => {
-  const extname = path.extname(originalname).toLowerCase();
-  const ext = extname.split('.').pop();
-  return exts.includes(ext as Ext);
+  type: (mimetype: Express.Multer.File['mimetype']) => {
+    const type = mimetype.split('/')[1];
+    return type;
+  },
+
+  name: (originalname: Express.Multer.File['originalname']) => {
+    const name = path.basename(originalname, file.ext(originalname));
+    return name;
+  },
+
+  isSupported: (
+    originalname: Express.Multer.File['originalname'],
+    exts: string[]
+  ) => {
+    const ext = file.ext(originalname).split('.').pop();
+
+    if (!ext) {
+      throw new Error('Invalid file. No extension specified');
+    }
+
+    const includes = exts.includes(ext);
+
+    if (!includes) {
+      throw new Error(`Unsupported file extension: ${ext}`);
+    }
+
+    return includes;
+  },
 };
 
-export { isSupported };
+export default file;
