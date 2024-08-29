b4a47bd3a5fa177b611ae894e7fa8937940a6ff9
devbylanre <devbylanre@gmail.com>
2024-08-29 02:32:18 +0100

    updated media model

--- src/models/media.ts
+++ src/models/media.ts
@@ -4,11 +4,11 @@ export type Media = {
   _id: Types.ObjectId;
   uri: string;
   name: string;
-  hash: string;
-  path: string;
   size: number;
   alt: string;
-  user: Types.ObjectId;
+  type: string;
+  path: string;
+  uploadedBy: Types.ObjectId;
   createdAt: Date;
 };
 
@@ -16,11 +16,11 @@ const schema = new Schema<Media>({
   alt: { type: String, default: '' },
   uri: { type: String, required: true },
   name: { type: String, required: true },
-  path: { type: String, required: true },
   size: { type: Number, required: true },
-  hash: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, required: true, ref: 'Users' },
+  type: { type: String, required: true },
+  path: { type: String, required: true },
   createdAt: { type: Date, required: true, default: Date.now() },
+  uploadedBy: { type: Schema.Types.ObjectId, required: true, ref: 'Users' },
 });
 
 const Medias = model<Media>('Medias', schema);
