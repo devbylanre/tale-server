75af80fa28b73ff9a897bf83db876073470c8852
devbylanre <devbylanre@gmail.com>
2024-08-29 02:34:27 +0100

    added user capability confirmation

--- src/routes/mediaRoute.ts
+++ src/routes/mediaRoute.ts
@@ -2,41 +2,63 @@ import { NextFunction, Request, Response, Router } from 'express';
 import { multiple } from '../controllers/mediaController';
 import { upload } from '../middlewares/multerMiddleware';
 import tokenize from '../utils/token';
-import { User } from '../models/user';
+import Users from '../models/user';
+import Roles from '../models/role';
+import { Capability } from '../models/capability';
 
 const mediaRoute = Router();
 
 mediaRoute.post(
   '/',
-  (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
       const token = req.headers.authorization?.split(' ')[1];
 
       if (!token) {
-        throw new Error('Invalid authorization token');
+        throw new Error('Authorization token is missing');
       }
 
       const user = tokenize.verify(token, process.env.ACCESS_TOKEN as string);
 
-      // const ROLES: readonly User['role'][] = [
-      //   'admin',
-      //   'author',
-      //   'developer',
-      //   'reader',
-      // ];
+      if (!user) {
+        throw new Error('Invalid authorization token');
+      }
 
-      // req.user = user as Record<string, string>;
+      req.user = user;
 
-      // if (!ROLES.includes((user as User).role)) {
-      //   throw new Error('Unauthorized access');
-      // }
+      const userDocument = await Users.findOne({ _id: user.id });
+
+      if (!userDocument) {
+        throw new Error('We are unable to find user account');
+      }
+
+      const userRole = await Roles.findOne({ _id: userDocument.role }).populate(
+        'capabilities'
+      );
+
+      if (!userRole) {
+        throw new Error('User role could not be found');
+      }
+
+      const CAPABILITY = 'canCreateMedias';
+      const userCapabilities = userRole.capabilities as unknown as Capability[];
+
+      const isUserCapable = userCapabilities.some(
+        (capability) => capability.name === CAPABILITY
+      );
+
+      if (!isUserCapable) {
+        throw new Error(
+          'Access revoked: You cannot access the resource ' + userRole.name
+        );
+      }
 
       next();
     } catch (error) {
       res.status(500).json({ message: (error as Error).message });
     }
   },
-  upload.array('medias'),
+  upload.array('files'),
   multiple
 );
 
